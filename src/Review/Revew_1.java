1862e9f0cc8b17318eedcacbe0e7a65333c747f8
dema28 <dema28ster@gmail.com>
2024-09-04 04:03:43 +0200

    Revew

---
+++ src/Review/Revew_1.java
@@ -0,0 +1,15 @@
+package Review;
+
+public class Revew_1 {
+    public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+    int sum = 0;
+        for (int i = 0; i < 3; i = i + 1) {
+        sum = sum + a;
+
+        }
+        System.out.println(sum);
+    }
+
+}
