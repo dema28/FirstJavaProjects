1aa520ccd0e04618187c271e6aa5f8dc0044e090
dema28 <dema28ster@gmail.com>
2024-09-07 20:20:48 +0200

    Работа в не урока

--- src/Helpers/Helper.java
+++ src/Helpers/Helper.java
@@ -5,8 +5,6 @@ public class Helper {public static String compare(Integer a, Integer a_1, Double
     String result = "___________________________________________________________\n";
     result += String.format("|%-20s|%-20s|%15s|%n", "HW8", "HW8_1", "Total");
     result += "___________________________________________________________\n";
-
-
     result += String.format("|%12s%-8d|%-14s%-6d|%15b|%n", "Integer a = ", a, "Integer a_1 = ", a_1, (a.equals(a_1)));
     result += "___________________________________________________________\n";
     result += String.format("|%-11s%-9.1f|%-13s%-7.1f|%15b|%n", "Double b = ", b, "Double b_1 = ", b_1, (b.equals(b_1)));
